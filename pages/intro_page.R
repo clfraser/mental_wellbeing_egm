9e6bb476334790bcb7c5366f9e87c75466640cfe
Catriona Fraser <catriona.fraser2@phs.scot>
2023-07-13 11:16:38 +0100

    Fixing typo

--- pages/intro_page.R
+++ pages/intro_page.R
@@ -4,7 +4,7 @@ output$intro_page_ui <-  renderUI({
   
   div(
     fluidRow(
-      h1("How to use an Evidence Gap Map (EGM)"),
+      h1("How to use an Evidence and Gap Map (EGM)"),
       h2("What is an EGM?"),
       p("An evidence and gap map is a new approach to presenting evidence for Public Health Scotland. It provides a visual representation of the evidence base on a specific policy area, highlighting where there is evidence and where there is not."),
       p("EGMs can be used to support decision-making in policy and practice, and to understand where there are gaps in knowledge and where research is needed."),
