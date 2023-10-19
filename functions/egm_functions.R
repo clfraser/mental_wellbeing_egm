f02338c3d0b6381cf49a89d3ba0ebd31b0b470d0
Catriona Fraser <catriona.fraser2@phs.scot>
2023-10-19 18:38:59 +0100

    Testing changes to remove double counting

--- functions/egm_functions.R
+++ functions/egm_functions.R
@@ -8,7 +8,7 @@
 filtered <- eventReactive(input$filter_update, {
   reviews_chart %>%
     mutate(selected = 0,
-           selected = if_else(  dummy == 0 &
+           selected = if_else(  (dummy == 0 &
                                 outcome_definition %in% input$outcome_def &
                                 age %in% input$pop_age &
                                 (sub_population %in% input$pop_characteristics | ("General population" %in% input$pop_characteristics & is.na(sub_population))) &
@@ -20,17 +20,24 @@ filtered <- eventReactive(input$filter_update, {
                                 type_of_review %in% input$synth_type_input &
                                 (input$qual_appraisal_input == "No" | (input$qual_appraisal_input == "Yes" & quality_appraisal == "Yes")) &
                                 (input$pre_reg_input == "No" | (input$pre_reg_input == "Yes" & pre_registered_protocol == "Yes")) &
-                                design_of_reviewed_studies %in% input$study_design_input,
+                                design_of_reviewed_studies %in% input$study_design_input) |
+                                overall_outcome %in% c("Outcome category 2", "Outcome category 3"),
                               1,
                               0))
 }, ignoreNULL = FALSE)
 
+only_selected <- reactive({filtered() %>%
+  filter(selected == 1)})
   
 output$egm <- renderReactable({
     ## create EGM plot    
-    grouped <- filtered() %>%
-      group_by(domain, subdomain, overall_outcome, intervention_exposure_short) %>%
-      summarise(count = sum(selected)) %>%
+    grouped <- reviews_table %>%
+      filter(study_id %in% only_selected()$study_id) %>%
+      separate_longer_delim(intervention_or_exposure, delim = "; ") %>%
+      separate_longer_delim(overall_domain, delim = "; ") %>%
+      separate_longer_delim(subdomain, delim = "; ") %>%
+      group_by(overall_domain, subdomain, overall_outcome, intervention_or_exposure) %>%
+      summarise(count = n()) %>%
       ungroup()
     
     ## Create a reactable
@@ -38,29 +45,30 @@ output$egm <- renderReactable({
     # Pivot data into the right format
     count_pivot <- grouped %>%
       mutate(overall_outcome = gsub(" |-", "_", overall_outcome)) %>% # Replace spaces and hyphens with underscores for better variable names (hyphens seem to cause problems with grouping columns below)
-      pivot_wider(names_from = c(overall_outcome, intervention_exposure_short), values_from = count, names_sep = ".") %>%
-      mutate(across(Self_harm.Exposure:Outcome_category_3.Exposure, ~replace_na(., 0))) %>% # Replace NAs with 0
-      select(-Self_harm.NA)
+      pivot_wider(names_from = c(overall_outcome, intervention_or_exposure), values_from = count, names_sep = ".") %>%
+      mutate(across(Self_harm.Exposure:Outcome_category_3.Intervention, ~replace_na(., 0))) # Replace NAs with 0
+    #%>%
+      #select(-Self_harm.NA)
     
     count_pivot %>%
       reactable(
         defaultColDef = colDef(
           align = 'center',
           maxWidth = 150),
-        groupBy = "domain",
+        groupBy = "overall_domain",
         defaultExpanded = TRUE,
         onClick = JS("function(rowInfo, column) {
-        // Don't handle click events in the domain or subdomain columns
-    if (column.id === 'domain' || column.id === 'subdomain') {
-      return
-    }
+        // Don't handle click events in the overall domain or subdomain columns
+        if (column.id === 'overall_domain' || column.id === 'subdomain') {
+          return
+        }
     // Send the click event to Shiny, which will be available in input$click_details
     if (window.Shiny) {
       Shiny.setInputValue('click_details', { subdomain: rowInfo.values.subdomain, outcome_and_type: column.id }, { priority: 'event' })
     }
   }"),
         columns = list(
-          domain = colDef(name = "Domain",
+          overall_domain = colDef(name = "Domain",
                           maxWidth = 150,
                           # Render grouped cells without the row count
                           grouped = JS("function(cellInfo) {
@@ -129,19 +137,15 @@ table_data <- reactive({
   outcome_click <- sub("_", "-", outcome_extract) # The outcome has an underscore in the original but we need it to have a hyphen for filtering
   type_click <- sub(".*\\.", "", input$click_details$outcome_and_type)
   
-  only_selected <-
-    filtered() %>%
-    filter(selected == 1)
-  
   if(is.null(input$click_details) | is.na(is.null(input$click_details))){
     return(reviews_table %>%
-             filter(study_id %in% only_selected$study_id) %>%
+             filter(study_id %in% only_selected()$study_id) %>%
              dplyr::select(study_id, title, aim_of_study, author_conclusions = summary, overall_outcome, outcome_definition, age, overall_population, sub_population, intervention_or_exposure, intervention_classification, study_setting, overall_domain, subdomain, type_of_review, design_of_reviewed_studies, number_of_primary_studies, empty_review, DOI) %>%
              arrange(study_id))
   }
   
   return(reviews_table %>%
-           filter(study_id %in% only_selected$study_id) %>%
+           filter(study_id %in% only_selected()$study_id) %>%
            arrange(study_id) %>%
            filter(str_detect(subdomain, input$click_details$subdomain) &
                     overall_outcome == outcome_click &
