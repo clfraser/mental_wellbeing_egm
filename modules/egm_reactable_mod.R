1c59156005e93cb80d0b331955bd5460486f7e78
clfraser <catriona.fraser2@phs.scot>
2025-03-12 16:48:05 +0000

    Add in filters

--- modules/egm_reactable_mod.R
+++ modules/egm_reactable_mod.R
@@ -7,27 +7,19 @@
 ## READ IN DATA ----
 #######################################################
 
-## Self-harm
-
-# Dataframe for plot
-self_harm_chart_data <- readRDS(here("data/self-harm_egm_chart_data.rds"))
-
-# Dataframe for table
-self_harm_table_data <- readRDS(here("data/self-harm_egm_table_data.rds"))
-
 ## WEMWEBS
 
 # Dataframe for adult plot
-wemwebs_adult_chart_data <- readRDS(here("data/wemwebs_adult_chart_data.parquet"))
+wemwebs_adult_chart_data <- read_parquet(here("data/wemwebs_adult_chart_data.parquet"))
 
 # Dataframe for adult table
-wemwebs_adult_table_data <- readRDS(here("data/wemwebs_adult_table_data.parquet"))
+wemwebs_adult_table_data <- read_parquet(here("data/wemwebs_adult_table_data.parquet"))
 
 # Dataframe for CYP plot
-wemwebs_cyp_chart_data <- readRDS(here("data/wemwebs_cyp_chart_data.parquet"))
+wemwebs_cyp_chart_data <- read_parquet(here("data/wemwebs_cyp_chart_data.parquet"))
 
 # Dataframe for CYP table
-wemwebs_cyp_table_data <- readRDS(here("data/wemwebs_cyp_table_data.parquet"))
+wemwebs_cyp_table_data <- read_parquet(here("data/wemwebs_cyp_table_data.parquet"))
 
 #######################################################
 ## MODULE UI ----
@@ -37,30 +29,172 @@ egm_reactable_ui <- function(id, dataset){
   ns <- NS(id)
   
   tagList(
-          reactableOutput(ns("egm_chart"))
-        )
+    
+    sidebarLayout(
+      
+      #######################################################
+      ## Filters in sidebar ----
+      #######################################################
+      
+      sidebarPanel(width = 3,
+                   ## Inputs
+                   # Set to use Shinyjs and set ID, so that checkboxes can be automatically reset
+                   shinyjs::useShinyjs(),
+                   id = ns("filter_panel"),
+                   
+                   # Action button to update filters
+                   actionButton(
+                     ns("filter_update_top"),
+                     "Apply filters"
+                   ),
+                   
+                   # Action button to select all filter options
+                   actionButton(
+                     ns("clear_all_filters_top"),
+                     "Clear all"
+                   ),
+                   
+                   linebreaks(2),
+                   
+                   div(id = ns("all_filters"), # Create div for the Cicerone tour
+                       
+                       # Domains and subdomains
+                       # Different for adult and CYP
+                       tags$span(
+                         tags$label("Select domain and sub-domain"), 
+                         actionButton(ns("domains_defs"), "", icon = icon("circle-info", `aria-label` = "Click for more information about domains and subdomains"))),
+                       # Domains and sub-domains with jsTreeR
+                       jstreeOutput(ns("domains_tree")),
+                       
+                       # Intervention or exposure. This is a straightforward checkboxGroupInput, since it doesn't need multiple levels.
+                       checkboxGroupInput(
+                         inputId = ns("intervention_risk_input"),
+                         label = tags$span("Select studies looking at interventions or risk/protective factors",
+                                           actionButton(ns("int_exposure_defs"), "", icon = icon("circle-info", `aria-label` = "Click for more information about interventions and risk/protective factors"))),
+                         choices = c("Intervention", "Risk/protective factor"),
+                         selected = NULL
+                       )
+                   ) # div
+                       
+      ), # sidebarPanel
+      
+      #######################################################
+      ## EGM chart in main panel ----
+      #######################################################
+      
+      mainPanel(
+        # Show chart_data to debug
+        #tableOutput(ns("chart_data_test")),
+        
+        reactableOutput(ns("egm_chart")))
+          
+      ) # sidebarLayout
+  ) #tagList
 }
+
+#######################################################
+## MODULE SERVER ----
+#######################################################
 
 egm_reactable_server <- function(id, dataset) {
   moduleServer(
     id,
     
-    function(input, output, server){
-      # Define full dataframe
+    function(input, output, session){
+      
+      # Define ns here since the jsTreeUpdate function for filters below refers to a namespaced output
+      ns <- session$ns
+      
+      #######################################################
+      ## Setting up dataset ----
+      #######################################################
+      
+      # Set the appropriate dataset, depending on the user input
     
-      full_dataframe <- dataset %>%
-                        mutate(selected = if_else(dummy == 0, 1, 0))
-    
-      # Create a reactive value for chart data and set it as the full dataframe initially
-    
+      selected_dataset <- reactive({
+        switch({dataset()},
+               "Adults" = wemwebs_adult_chart_data,
+               "Children and young people" = wemwebs_cyp_chart_data)
+      })
+      
+      # Create a reactiveVal for chart_data
       chart_data <- reactiveVal()
-      chart_data(full_dataframe)
+      
+      # Set chart data to the full dataframe initially, and add a selected column
+      full_dataframe <- reactive({selected_dataset() %>%
+            mutate(selected = if_else(dummy == 0, 1, 0))})
+      
+      # Set chart_data to full_dataframe
+      observe({chart_data(full_dataframe())})
+      
+      #######################################################
+      ## Filters ----
+      #######################################################
+      
+      # Putting data in the right format for jsTreeR
+      
+      # Create initial dataframes for trees
+      
+      domains_subs_for_tree <- reactive({selected_dataset() %>%
+          select(domain, subdomain) %>%
+          distinct(domain, subdomain) %>%
+          arrange(domain, subdomain)
+      })
+      
+      # Turn these into dataframes for creating nodes for the tree
+      # Note that for the create_df_for_nodes function, the column names shouldn't be in quotes,
+      # but for the create_nodes_from_df function, they should
+      
+      domains_df <- reactive({create_df_for_nodes(domains_subs_for_tree(), subdomain)})
+      
+      # Turn the dataframes into nodes
+      
+      domain_subs_nodes <- reactive({create_nodes_from_df(domains_df(), "domain", "subdomain")})
+      
+      ## Render tree for jsTreeR inputs
+      output$domains_tree <- renderJstree({
+        jstree(
+          domain_subs_nodes(),
+          checkboxes = TRUE
+        )
+      })
+      
+      # Reset filters when Clear Filters button clicked
+      # Reset from the shinyjs package doesn't reset the tree (hierarchical) checkboxes, so add these separately
+      # Assign full_dataframe to chart_data when Clear Filters button is clicked
+      
+      observeEvent(input$clear_all_filters_top, {
+        shinyjs::reset("filter_panel")
+        jstreeUpdate(session, ns("domains_tree"), clear_tree(domains_df(), "domain", "subdomain"))
+
+        # Set chart_data to full_dataframe (remove any filtering on the chart data)
+        chart_data(full_dataframe())
+      })
+      # 
+      # Filtered dataframe
+      
+      observeEvent(input$filter_update_top, {
+        chart_data(selected_dataset() %>%
+                     mutate(domains_filter = if(is.null(unlist(input$domains_tree_selected))) TRUE else if_else(subdomain %in% unlist(input$domains_tree_selected), TRUE, FALSE),
+                            intervention_risk_filter = if(is.null(input$intervention_risk_input)) TRUE else if_else(intervention_exposure_short %in% input$intervention_risk_input, TRUE, FALSE),
+                            selected = if_else(domains_filter + intervention_risk_filter
+                                               + dummy == 2, 1, 0))) # All of the filter checks are true, but the record isn't a dummy one
+        })
+      
+      
+      output$chart_data_test <- renderTable({chart_data()})
+
+      #######################################################
+      ## Formatting data for EGM ----
+      #######################################################
+      
+      observe({
       
       # Get data into the right format for the EGM
       count_pivot <- reactive({
         chart_data() %>%
           group_by(domain, subdomain, overall_outcome, intervention_exposure_short) %>%
-          summarise(count = length(unique(covidence_number[selected == 1]))) %>% # Count the covidence numbers (unique IDs) for studies that have been selected
+          summarise(count = length(unique(id_number[selected == 1]))) %>% # Count the unique IDs for studies that have been selected
           ungroup() %>%
           mutate(overall_outcome = gsub(" |-", "_", overall_outcome),
                  intervention_exposure_short = gsub(" |-|/", "_", intervention_exposure_short)) %>% # Replace spaces, slashes and hyphens with underscores for better variable names (hyphens seem to cause problems with grouping columns below)
@@ -68,15 +202,15 @@ egm_reactable_server <- function(id, dataset) {
           mutate(across(everything(), ~replace_na(., 0))) %>% # Replace NAs with 0
           arrange(domain) %>%
           mutate(padding = "") %>%
-          select(padding, domain, subdomain, Self_harm.Risk_protective_factor, Self_harm.Intervention)
+          select(padding, domain, subdomain, WEMWEBS.Risk_protective_factor, WEMWEBS.Intervention)
       })
-      
+
       # Create aggregated EGM table
-      
+
       egm_aggregated_count <- reactive({
         chart_data() %>%
           group_by(domain, overall_outcome, intervention_exposure_short) %>%
-          summarise(count = length(unique(covidence_number[selected == 1]))) %>% # Count the covidence numbers (unique IDs) for studies that have been selected
+          summarise(count = length(unique(id_number[selected == 1]))) %>% # Count the unique IDs for studies that have been selected
           ungroup() %>%
           mutate(overall_outcome = gsub(" |-", "_", overall_outcome),
                  intervention_exposure_short = gsub(" |-|/", "_", intervention_exposure_short)) %>% # Replace spaces, slashes and hyphens with underscores for better variable names (hyphens seem to cause problems with grouping columns below)
@@ -84,21 +218,23 @@ egm_reactable_server <- function(id, dataset) {
           mutate(across(everything(), ~replace_na(., 0))) %>% # Replace NAs with 0
           arrange(domain) %>%
           mutate(subdomain = "") %>%
-          select(domain, subdomain, Self_harm.Risk_protective_factor, Self_harm.Intervention)
+          select(domain, subdomain, WEMWEBS.Risk_protective_factor, WEMWEBS.Intervention)
       })
-      
+
       # Create table with both aggregated and disaggregated data
-      
+
       egm_agg_disag <- reactive({
         count_pivot() %>%
           select(-padding) %>%
           rbind(egm_aggregated_count())
       })
-      
-      # Create EGM plot
-      
-      output$egm <- renderReactable({
-        
+
+      #######################################################
+      ## Creating EGM plot ----
+      #######################################################
+
+      output$egm_chart <- renderReactable({
+
         reactable(
           egm_aggregated_count(),
           # Only show shapes in header rows when they are expanded. See buttons.css for the CSS.
@@ -117,14 +253,14 @@ egm_reactable_server <- function(id, dataset) {
           columns = list(
             domain = colDef(name = "Domain"),
             subdomain = colDef(name = "Sub-domain"),
-            Self_harm.Risk_protective_factor = colDef(name = "Risk/protective factor",
+            WEMWEBS.Risk_protective_factor = colDef(name = "Risk/protective factor",
                                                       cell = bubble_grid_modified(
                                                         data = egm_agg_disag(),
                                                         colors = '#83BB26',
                                                         tooltip = TRUE,
                                                         shape = "squares"
                                                       )),
-            Self_harm.Intervention = colDef(name = "Intervention",
+            WEMWEBS.Intervention = colDef(name = "Intervention",
                                             cell = bubble_grid_modified(
                                               data = egm_agg_disag(),
                                               colors = '#3F3685',
@@ -150,7 +286,7 @@ egm_reactable_server <- function(id, dataset) {
                                         width = 150),
                         subdomain = colDef(name = "Sub-domain",
                                            width = 150),
-                        Self_harm.Risk_protective_factor = colDef(name = "",
+                        WEMWEBS.Risk_protective_factor = colDef(name = "",
                                                                   vAlign = "bottom",
                                                                   cell = bubble_grid_modified(
                                                                     data = egm_agg_disag(),
@@ -158,7 +294,7 @@ egm_reactable_server <- function(id, dataset) {
                                                                     tooltip = TRUE,
                                                                     shape = "squares"
                                                                   )),
-                        Self_harm.Intervention = colDef(name = "",
+                        WEMWEBS.Intervention = colDef(name = "",
                                                         vAlign = "top",
                                                         cell = bubble_grid_modified(
                                                           data = egm_agg_disag(),
@@ -167,10 +303,27 @@ egm_reactable_server <- function(id, dataset) {
                                                         ))
                       ))
           }
-          
+
         )
       }) # renderReactable
+      
+      }) # observe
     
     }
   )
 }
+
+# Run the app to test
+
+ui <- fluidPage(
+  selectInput("dataset_input", "Select population of interest:",
+              choices = c("Adults", "Children and young people")),
+  
+  egm_reactable_ui("egm_reactable", dataset = reactive({input$dataset_input}))
+)
+
+server <- function(input, output, session) {
+  egm_reactable_server("egm_reactable", dataset = reactive({input$dataset_input}))
+}
+
+shinyApp(ui, server)
